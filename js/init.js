051fe253cedb78fcca0f74792c3029c72ab8a537
bhenriquez8 <bhenri88@gmail.com>
2020-05-17 13:38:24 -0700

    added images for front page

--- js/init.js
+++ js/init.js
@@ -1,8 +1,7 @@
-(function($){
-  $(function(){
-
+(function ($) {
+  $(function () {
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $('select').formSelect();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
